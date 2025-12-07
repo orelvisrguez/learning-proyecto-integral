4e7c4546486a0d9ec4489bef8bcd18438d79ce67
orelvis <orelvis.rguez@gmail.com>
2025-12-07 01:24:22 -0300

    actualizando a astro vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   site: 'https://learning-proyecto-integral.edu.ar',
   compressHTML: true,
   build: {
