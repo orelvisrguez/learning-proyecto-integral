a59325f2c845e771e84db3baf180da2f3772c3ff
orelvis <orelvis.rguez@gmail.com>
2026-03-03 16:57:43 -0300

    degradando a prisma por incompatible

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,23 +1,9 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
-
-const connectionString = process.env.DATABASE_URL
-
-function createPrismaClient() {
-  if (!connectionString) {
-    throw new Error('DATABASE_URL is not defined')
-  }
-  
-  const pool = new Pool({ connectionString })
-  const adapter = new PrismaPg(pool)
-  return new PrismaClient({ adapter })
-}
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? createPrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
